9f095618a3ca93917ecfd5a756eed5736bc72b17
shinnh2 <35826626+shinnh2@users.noreply.github.com>
2025-06-30 11:59:23 +0900

    Update task_truthyFalsy_신나현.js

--- conditional/task_truthyFalsy_신나현.js
+++ conditional/task_truthyFalsy_신나현.js
@@ -1,1 +1,63 @@
-sd
+//[]
+if([]){
+    console.log("truthy");
+}else{
+    console.log("falsy");
+}
+//결과: truthy
+
+//[].length
+if([].length){
+    console.log("truthy");
+}else{
+    console.log("falsy");
+}
+//결과: falsy
+
+//{}
+if({}){
+    console.log("truthy");
+}else{
+    console.log("falsy");
+}
+//결과: truthy
+
+//""
+if(""){
+    console.log("truthy");
+}else{
+    console.log("falsy");
+}
+//결과: falsy
+
+//0
+if(0){
+    console.log("truthy");
+}else{
+    console.log("falsy");
+}
+//결과: falsy
+
+//1
+if(1){
+    console.log("truthy");
+}else{
+    console.log("falsy");
+}
+//결과: truthy
+
+//null, undefined
+if(null){
+    console.log("truthy");
+}else{
+    console.log("falsy");
+}
+//결과: falsy
+
+//undefined
+if(undefined){
+    console.log("truthy");
+}else{
+    console.log("falsy");
+}
+//결과: falsy
